5921d6020ffe9307564b1aeec43c4e5a51bc2850
Sarah Mellor <sarahmellor16@gmail.com>
2018-06-29 03:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase ()) return "I can't hear you!";
   else if (string === string.toUpperCase ()) return "YES INDEED!";
-  else if (string === string.mixedCase()) return "I love you, too."
+  else if (string === string.mixedCase.toLowerCase()) return "I love you, too."
 }
 
 
